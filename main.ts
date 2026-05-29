c4376b034fef4887b282538f2a22978e0b4a41c6
matsv8x <matsvanagtmael2013@gmail.com>
2026-05-29 09:13:17 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,50 @@
+let IR_waarde = IR.IR_read()
+let links = 29
+let rechts = 16
+let vooruit = 8
+let achteruit = 5
+let stop = 141
+basic.forever(function () {
+    if (IR_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
